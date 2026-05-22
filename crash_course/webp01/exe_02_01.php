eee4008c960125dc865098999406dd6aaba83449
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 06:59:37 +0100

    deleted files

--- crash_course/webp01/exe_02_01.php
+++
@@ -1,6 +0,0 @@
-<?php
-$scoreFloat = 55.9;
-$scoreInt = (int) $scoreFloat;
-print gettype($scoreFloat) . ", scoreFloat = " . $scoreFloat . "\n";
-print gettype($scoreInt) . ", scoreInt = " . $scoreInt . "\n";
-?>
