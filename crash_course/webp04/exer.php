1de01acee36e8660a8d197a9b7754cbcd0f88089
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 12:41:44 +0100

    using conditional statements in php

--- crash_course/webp04/exer.php
+++ crash_course/webp04/exer.php
@@ -1,5 +1,36 @@
-
 <?php
-    print "Hello, World\n";
-?>
+// 1
+$name = "erlang";
+$shortLength = 4;
+print strlen($name) < $shortLength
+    ? "$name is too short.\n"
+    : "$name is enough.\n";
 
+// 2
+$laundryWeight = 9;
+$standardWeight = 10;
+print $laundryWeight > $standardWeight
+    ? "Fits in standard machine.\n"
+    : "Need medium to large machine.\n";
+
+// 3
+
+$vehicleKind = "helicopter";
+// can also use a switch statement instead of a match expression
+print match ($vehicleKind) {
+    "bus" => "Beep beep!\n",
+    "train" => "Run on tracks.\n",
+    "helicopter" => "Can fly.\n",
+    "bicycle" => "You never forget once you've learned!\n",
+    default => "You've chosen the road less travelled.\n",
+};
+
+// 4
+$userCorrectName = "password";
+$passwordCorrect = "87654321";
+
+if ($userCorrectName === $name && $passwordCorrect === $laundryWeight) {
+    print "You are now logged in.\n";
+} else {
+    print "Invalid credentials, please try again.\n";
+}
