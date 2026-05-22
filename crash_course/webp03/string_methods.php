572551ad2b9c6049b7a16383ce500c099b03b526
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 09:19:12 +0100

    practice with string

--- crash_course/webp03/string_methods.php
+++ crash_course/webp03/string_methods.php
@@ -1,5 +1,18 @@
 
 <?php
-    print "Hello, World\n";
-?>
+$str = "the Cat sat on the Mat";
 
+print "Original statement: \n\t" . $str . "\n";
+print "\t0123456789012345678901\n";
+print "Uppercase: " . strtoupper($str) . "\n";
+print "Lowercase: " . strtolower($str) . "\n";
+print "Ucfirst: " . ucfirst($str) . "\n";
+print "Ucwords: " . ucwords($str) . "\n";
+
+print "String position 'on': " . strpos($str, "on") . "\n";
+print "The length of the string: " . strlen($str) . "\n";
+
+print 'Count number of the "at": ' . substr_count($str, "at") . "\n";
+
+
+?>
