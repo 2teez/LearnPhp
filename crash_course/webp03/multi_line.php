2fc42ba28ebf8cb563cc4948ae18ca1c9d4e88af
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 07:31:10 +0100

    using unicode in php

--- crash_course/webp03/multi_line.php
+++ crash_course/webp03/multi_line.php
@@ -5,7 +5,16 @@ print '
 My name is ' .
     $name .
     ' and I\'m pleased to be learning PHP. ' .
-    "\nPHP is a server-side scripting language" .
-    " that is easy to learn and use." .
-    PHP_EOL;
+    PHP_EOL .
+    "PHP is a server-side scripting language" .
+    " that is easy to learn and use.\n";
+
+echo "=====================\n";
+$smiley = "\u{1F60A}";
+$elephant = "\u{1F418}";
+$cherokeeTSV = "\u{13E8}";
+
+print "this is a smiley face: \n$smiley\n";
+print "this is an elephant: \n$elephant\n";
+print "this is a cherokee TSV: \n$cherokeeTSV\n";
 ?>
