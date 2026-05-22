19e24513936db164e68ded1bf61a1df7837cc374
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 14:45:59 +0100

    using absolute path in php

--- crash_course/webp05/main.php
+++ crash_course/webp05/main.php
@@ -1,8 +1,12 @@
-
 <?php
 print "I'm in main php file\n";
+// using relative path
+//require_once "file2.php";
+// using absolute path
+$callingScriptPath = __DIR__;
+print "calling script path: $callingScriptPath\n";
 
-require_once "file2.php";
+require_once __DIR__ . "/file2.php";
 
 print "Back in main.php file\n";
 
