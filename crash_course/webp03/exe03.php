f79a43971f0244e1cdcabc9e06998ca987d35e48
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 10:21:38 +0100

    practiced with string complete

--- crash_course/webp03/exe03.php
+++ crash_course/webp03/exe03.php
@@ -1,5 +1,33 @@
 <?php
+// 1
 $name = "clojure";
-print $name . " is learning PHP";
+print $name . " is learning PHP\n";
+echo "\n";
+
+// Question 2 and 4
+$fruit = "apple";
+
+$fact = <<<END
+
+    $fruit juice is made from {$fruit}s.
+
+END;
+
+print ucfirst(ltrim($fact)) . "\n";
+
+$fact = str_replace("apple", "orange", $fact);
+
+print ucfirst(ltrim($fact)) . "\n";
+
+$happyMessage = <<<EOL
+
+PHP is fun!
+\u{1F60A}
+\u{1F60E}
+\u{1F642}
+
+EOL;
+
+print $happyMessage;
 
 ?>
