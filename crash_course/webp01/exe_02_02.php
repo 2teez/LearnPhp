9dcd2cb92443b01bf16ebf70f25b99a0590b8cd2
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 06:56:38 +0100

    corrected some values in the exercise script

--- crash_course/webp01/exe_02_02.php
+++ crash_course/webp01/exe_02_02.php
@@ -1,10 +1,10 @@
 <?php
 $age = 21;
-print var_dump($age);
+var_dump($age);
 echo "Setting age to null\n";
 $age = null;
-print var_dump($age);
+var_dump($age);
 echo "Unsetting age\n";
 unset($age);
-print var_dump($age);
+var_dump($age);
 ?>
