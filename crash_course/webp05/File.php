8365fbd061e4a457322a8c579059945204e4040c
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 21:27:50 +0100

    complete exercise

--- crash_course/webp05/File.php
+++ crash_course/webp05/File.php
@@ -1,5 +1,28 @@
-
 <?php
-    print "Hello, World\n";
-?>
+function printNumbers(string $num = "456", string $line = ""): void
+{
+    print "$num$line";
+}
 
+function whichIsLarger(int|float $a, int|float $b): int|float|null
+{
+    $result = null;
+    if ($a > $b) {
+        $result = $a;
+    } elseif ($b > $a) {
+        $result = $b;
+    } else {
+        $result = null;
+    }
+    return $result;
+}
+
+function printResult(int|float|null $result): string
+{
+    if (!is_null($result)) {
+        return "$result";
+    } else {
+        return "null";
+    }
+}
+?>
--- crash_course/webp05/Main.php
+++ crash_course/webp05/Main.php
@@ -1,5 +1,14 @@
-
 <?php
-    print "Hello, World\n";
-?>
+require_once __DIR__ . "/File.php";
 
+// 1
+printNumbers("123");
+printNumbers();
+printNumbers("789", "\n");
+
+// 2 and 3
+print printResult(whichIsLarger(4, 5)) . "\n";
+print printResult(whichIsLarger(21, 19)) . "\n";
+print printResult(whichIsLarger(3, 3)) . "\n";
+
+?>
