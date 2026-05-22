592ce24ee8578221c0d180aafb1a5a8fc3c01b34
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 15:38:15 +0100

    calling the function file from the main file

--- crash_course/webp05/proj2/Main.php
+++ crash_course/webp05/proj2/Main.php
@@ -1,3 +1,9 @@
 <?php
-require_once __DIR__ . "Myfunction.php";
+require_once __DIR__ . "/Myfunction.php";
+
+$result = whichIsSmaller(5, 2);
+print "\nthe smaller of 5 and 2 = " . $result;
+
+$result = whichIsLarger(22, 5);
+print "\nthe larger of 22 and 5 = " . $result . PHP_EOL;
 ?>
