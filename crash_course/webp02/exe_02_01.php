0b2e50e13475a432cefdd9a2357e5af34c305259
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 06:58:28 +0100

    moved all the exercise files into the right folder

---
+++ crash_course/webp02/exe_02_01.php
@@ -0,0 +1,6 @@
+<?php
+$scoreFloat = 55.9;
+$scoreInt = (int) $scoreFloat;
+print gettype($scoreFloat) . ", scoreFloat = " . $scoreFloat . "\n";
+print gettype($scoreInt) . ", scoreInt = " . $scoreInt . "\n";
+?>
---
+++ crash_course/webp02/exe_02_02.php
@@ -0,0 +1,10 @@
+<?php
+$age = 21;
+var_dump($age);
+echo "Setting age to null\n";
+$age = null;
+var_dump($age);
+echo "Unsetting age\n";
+unset($age);
+var_dump($age);
+?>
