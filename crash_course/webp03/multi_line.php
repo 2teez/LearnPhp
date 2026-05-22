e1128b24cfeb831ebcacc0bb917dabf9c283fe33
timothy adigun <adigun.timothy@gmail.com>
2026-05-22 07:24:50 +0100

    using both single and double quation marks in php

---
+++ crash_course/webp03/multi_line.php
@@ -0,0 +1,11 @@
+<?php
+$name = "JavaScript";
+
+print '
+My name is ' .
+    $name .
+    ' and I\'m pleased to be learning PHP. ' .
+    "\nPHP is a server-side scripting language" .
+    " that is easy to learn and use." .
+    PHP_EOL;
+?>
