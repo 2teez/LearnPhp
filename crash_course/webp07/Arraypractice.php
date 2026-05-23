94e4ebd60c64715a75d7cbe8d6853f23abb14c44
timothy adigun <adigun.timothy@gmail.com>
2026-05-23 08:13:11 +0100

    playing with array foreach loop

--- crash_course/webp07/Arraypractice.php
+++ crash_course/webp07/Arraypractice.php
@@ -6,8 +6,19 @@ $index = intval(getUserInput("Enter an index of array to delete."));
 if (isset($animals[$index])) {
     print "(Removing index $index)";
     unset($animals[$index]);
-    var_dump($animals);
+    print_r($animals);
 } else {
-    print "Index not found.\n";
+    print "No element in index [$index].\n";
 }
+
+// loop thru an array
+foreach ($animals as $key => $value) {
+    if ($key == array_key_last($animals)) {
+        print "and [$key] => $value.\n";
+    } else {
+        print "[$key] => $value, ";
+    }
+}
+// using implode to join array elements
+print implode(", ", $animals) . "\n";
 ?>
