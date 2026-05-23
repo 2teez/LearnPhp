ba83237bc7000fdbe7d92b349f1910da1911f354
timothy adigun <adigun.timothy@gmail.com>
2026-05-23 21:04:49 +0100

    using php to do file and directory manipulation

--- crash_course/webp09/Main.php
+++ crash_course/webp09/Main.php
@@ -2,3 +2,35 @@
 $file = __DIR__ . "/data.txt";
 $content = file_get_contents($file);
 print $content;
+
+$employeesData = [
+    [
+        "name" => "miami",
+        "age" => 30,
+        "position" => "developer",
+    ],
+    ["name" => "lagos", "age" => 35, "position" => "tester"],
+];
+// check for the directory called data
+// if it doesn't exist, create it
+$dirPath = __DIR__ . "/data";
+if (!file_exists($dirPath)) {
+    mkdir($dirPath);
+}
+$jsonEmployeeFile = $dirPath . "/employee.json";
+$fileHandler = null;
+if (!file_exists($jsonEmployeeFile)) {
+    touch($jsonEmployeeFile);
+    $fileHandler = fopen($jsonEmployeeFile, "w");
+} else {
+    $fileHandler = fopen($jsonEmployeeFile, "a");
+}
+file_put_contents($jsonEmployeeFile, json_encode($employeesData));
+fclose($fileHandler);
+// read it back
+print_r(file_get_contents($jsonEmployeeFile));
+print "\n";
+// delete the employee.json file
+unlink($jsonEmployeeFile);
+// delete the data directory
+rmdir($dirPath);
