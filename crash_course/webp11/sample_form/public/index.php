35ca5f6deb699ef2f8e46de28545d6e16323a780
timothy adigun <adigun.timothy@gmail.com>
2026-05-27 07:31:50 +0100

    practice form

---
+++ crash_course/webp11/sample_form/public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html lang="en">
+<html>
+    <meta charset="utf-8">
+<head>
+    <title>Simple Form</title>
+</head>
+<body>
+    <form action="process.php" method="get">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+        <br><br>
+        <label for="age">Age:</label>
+        <input type="text" id="age" name="age">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
---
+++ crash_course/webp11/sample_form/public/process.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html lang="en">
+<html>
+<meta charset="utf-8">
+<head>
+    <title></title>
+</head>
+<body>
+<?php  ?>
+</body>
+</html>
