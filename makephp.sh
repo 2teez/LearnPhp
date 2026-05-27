a9fcefc3ea09ac732ead7eb4d3ef2d6eed1041fc
timothy adigun <adigun.timothy@gmail.com>
2026-05-27 15:22:22 +0100

    added all the link for bootstrap to the template used in the bashscript

--- makephp.sh
+++ makephp.sh
@@ -33,16 +33,25 @@ BOOTSTRAP_HTML="
 <!DOCTYPE html>
 <html lang=\"en\">
     <head>
-        <meta charset=\"utf-8\">
+    <!-- Required meta tags always come first -->
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1,
+    shrink-to-fit=no\">
+    <meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\">
         <title>${filename}</title>
+
+        <!-- Bootstrap CSS -->
         <link rel=\"stylesheet\"
         href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap
-        .min.css\"  crossorigin=\"anonymous\">
+        .min.css\"
+        crossorigin=\"anonymous\">
     </head>
     <body>
         <?php
-
+            echo \"Hello, World\n\";
         ?>
+
+        <!-- jQuery first, then Bootstrap JS. -->
         <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\"></script>
         <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js\"
             crossorigin=\"anonymous\"></script>
