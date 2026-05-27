1a7ac37c9e0abedd079afa4dcddaf543207649df
timothy adigun <adigun.timothy@gmail.com>
2026-05-27 11:29:53 +0100

    using bootstrap and button with name and class

--- crash_course/webp11/movies/public/index2.php
+++ crash_course/webp11/movies/public/index2.php
@@ -3,17 +3,25 @@
 <head>
     <meta charset="utf-8">
     <title>Movie Form 1</title>
+    <link rel="stylesheet"
+    href ="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    >
 </head>
 <body>
-    <h1>Edit Movie</h1>
-<form action="/process.php?id=1022" method="post">
-    <label for="title">Title:</label>
-    <input type="text" id="title" name="title">
-    <br><br>
-    <label for="price">Price:</label>
-    <input type="text" id="price" name="price">
-    <br><br>
-    <input type="submit" value="Submit">
+    <h1>Rent a Movie:</h1>
+<form action="/process.php?movieId=80441" method="post">
+    <p><label for="number">Credit Card Number:</label>
+        <input type="text" id="number" name="number">
+    </p>
+    <p><label for="date">Expiry Date:</label>
+        <input type="text" id="date" name="date">
+    </p>
+    <p><label for="ccv">CCV Code:</label>
+        <input type="text" id="ccv" name="ccv">
+    </p>
+
+    <input type="submit" value="Pay and start watching now" name="watchNow" class="btn btn-success">
+    <input type="submit" value="Pay and watch later" name="watchLater" class="btn btn-success">
 </form>
 </body>
 </html>
