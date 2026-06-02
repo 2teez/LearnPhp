af7a2cedbee61517bef1fb360966747c7ff24774
timothy adigun <adigun.timothy@gmail.com>
2026-06-02 21:43:38 +0100

    practice link in php

---
+++ crash_course/webp11/movies/public/index3.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Basket Form</title>
+
+        <!-- Bootstrap CSS -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+        >
+    </head>
+    <body class="container">
+        <div class="row">
+            <div class="col-lg-3 text-end py-3">
+                Hammer £19.99
+                <a href="show.php?id=102" class="btn btn-primaary">Details</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 text-end py-3">
+                Bucket of Nails £9.99
+                <a href="show.php?id=115" class="btn btn-primaary">Details</a>
+            </div>
+        </div>
+        <!-- Bootstrap JS. -->
+        <script
+            src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js>
+        </script>
+    </body>
+</html>
