d99cf9c588142f588ec55cea5f75a6f18dd0b992
timothy adigun <adigun.timothy@gmail.com>
2026-06-02 21:27:39 +0100

    css formatting using bs5

--- crash_course/webp11/movies/public/index.php
+++ crash_course/webp11/movies/public/index.php
@@ -1,3 +1,11 @@
+<?php
+if (!isset($title)) {
+    $title = "";
+}
+if (!isset($price)) {
+    $price = "";
+}
+?>
 <!DOCTYPE html lang="en">
 <html>
 <head>
@@ -8,10 +16,14 @@
     <h1>Edit Movie</h1>
 <form action="/process.php?id=1022" method="post">
     <label for="title">Title:</label>
-    <input type="text" id="title" name="title">
+    <input type="text" id="title" name="title" value="<?= htmlspecialchars(
+        $title,
+    ) ?>">
     <br><br>
     <label for="price">Price:</label>
-    <input type="text" id="price" name="price">
+    <input type="text" id="price" name="price" value="<?= htmlspecialchars(
+        $price,
+    ) ?>">
     <br><br>
     <input type="submit" value="Submit">
 </form>
