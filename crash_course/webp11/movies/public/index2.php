4447126ff82a753dc45555a1ffb4e672521f5345
timothy adigun <adigun.timothy@gmail.com>
2026-06-02 21:26:25 +0100

    added bs formatting

--- crash_course/webp11/movies/public/index2.php
+++ crash_course/webp11/movies/public/index2.php
@@ -8,17 +8,17 @@
     >
 </head>
 <body>
-    <h1>Rent a Movie:</h1>
-<form action="/process.php?movieId=80441" method="post">
-    <p><label for="number">Credit Card Number:</label>
-        <input type="text" id="number" name="number">
-    </p>
-    <p><label for="date">Expiry Date:</label>
-        <input type="text" id="date" name="date">
-    </p>
-    <p><label for="ccv">CCV Code:</label>
-        <input type="text" id="ccv" name="ccv">
-    </p>
+    <h1 class="text-center p-3 display-1">Rent a Movie:</h1>
+    <form class="text-center" action="/process.php?movieId=80441" method="post">
+        <p><label for="number" class="text-start">Credit Card Number:</label>
+            <input type="text" id="number" name="number">
+        </p>
+        <p><label for="date" class="text-start">Expiry Date:</label>
+            <input type="text" id="date" name="date">
+        </p>
+        <p><label for="ccv" class="text-start">CCV Code:</label>
+            <input type="text" id="ccv" name="ccv">
+        </p>
 
     <input type="submit" value="Pay and start watching now" name="watchNow" class="btn btn-success">
     <input type="submit" value="Pay and watch later" name="watchLater" class="btn btn-success">
