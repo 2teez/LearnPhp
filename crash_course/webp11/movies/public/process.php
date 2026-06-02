31d018070295db28eed7e766d3b4cf04178db776
timothy adigun <adigun.timothy@gmail.com>
2026-06-02 21:26:59 +0100

    add style to process file

--- crash_course/webp11/movies/public/process.php
+++ crash_course/webp11/movies/public/process.php
@@ -8,13 +8,23 @@ $price = filter_input(INPUT_POST, "price");
 <head>
     <meta charset="utf-8">
     <title>Process</title>
-</head>
-<body>
-Id = <?= $id ?>
-<br>
-Title = <?= $title ?>
-<br>
-Price = <?= $price ?>
-
-</body>
+    <!-- Bootstrap CSS -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+    >
+    </head>
+    <body class="p-3">
+        <main>
+            <span>Id = <?= $id ?></span>
+            <br />
+            <span>Title = <?= $title ?></span>
+            <br />
+            <span>Price = <?= $price ?></span>
+            <br />
+            <script
+                src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js>
+            </script>
+        </main>
+    </body>
 </html>
