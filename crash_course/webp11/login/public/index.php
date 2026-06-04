e372717e8c675a6e37a45e0d980fc5bd2df7784d
timothy adigun <adigun.timothy@gmail.com>
2026-06-04 15:18:04 +0100

    exercise 2 done

--- crash_course/webp11/login/public/index.php
+++ crash_course/webp11/login/public/index.php
@@ -1,4 +1,7 @@
-
+<?php
+$username = filter_input(INPUT_POST, "username") ?? "";
+$password = filter_input(INPUT_POST, "password") ?? "";
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -13,10 +16,25 @@
             rel="stylesheet"
         >
     </head>
-    <body>
-        <?php
-            echo "Hello, World\n";
-        ?>
+    <body class="p-3">
+        <h1 class="mb-3 display-4 border-4 fw-bold">Login</h1>
+        <form action="login.php" method="post">
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" name="username" placeholder="Username" value="<?= htmlspecialchars(
+                    $username,
+                ) ?>" required />
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" placeholder="Password" value="<?= htmlspecialchars(
+                    $password,
+                ) ?>" required />
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
 
         <!-- Bootstrap JS. -->
         <script
@@ -24,4 +42,3 @@
         </script>
     </body>
 </html>
-
