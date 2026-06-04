b8b20bb364240dcc33d381d1f11b28448454f9c8
timothy adigun <adigun.timothy@gmail.com>
2026-06-04 14:57:21 +0100

    change the name of the php file

---
+++ crash_course/webp11/costumeRental/public/costumeSearch.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title></title>
+
+        <!-- Bootstrap CSS -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+        >
+    </head>
+    <body>
+        <?php echo "Hello, World\n"; ?>
+
+        <!-- Bootstrap JS. -->
+        <script
+            src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js>
+        </script>
+    </body>
+</html>
