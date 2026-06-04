fa50f719c382e2decfd959c27732c38bcccc03aa
timothy adigun <adigun.timothy@gmail.com>
2026-06-04 14:57:46 +0100

    updated the exercise index file

--- crash_course/webp11/costumeRental/public/index.php
+++ crash_course/webp11/costumeRental/public/index.php
@@ -1,3 +1,5 @@
+<?php
+$superhero = filter_input(INPUT_GET, "hero") ?? ""; ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -12,9 +14,18 @@
             rel="stylesheet"
         >
     </head>
-    <body>
-        <?php echo "Hello, World\n"; ?>
-
+    <body class="container p-3">
+        <form action="costumeRental.php" method="GET">
+                <div class="mb-3">
+                    <label for="name">Super Hero Costume: </label>
+                    <input type="text" id="name" name="hero" value="<?= htmlspecialchars(
+                        $superhero,
+                    ) ?>" required>
+                </div>
+                <div class="mb-3">
+                    <input type="submit" value="Submit">
+                </div>
+        </form>
         <!-- Bootstrap JS. -->
         <script
             src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js>
